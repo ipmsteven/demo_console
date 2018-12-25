c1f00a77e250998e4f26c1403a22fed7a74e0f45
ipmsteven <steven.lyl147@gmail.com>
2018-12-24 16:10:48 -0800

    hello world on web

--- Program.cs
+++ Program.cs
@@ -1,12 +1,22 @@
 ﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
 
 namespace console
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+      var config = new ConfigurationBuilder().AddCommandLine(args).Build();
+      var host = new WebHostBuilder()
+        .UseKestrel()
+        .UseStartup<Startup>()
+        .UseConfiguration(config)
+        .Build();
+
+      host.Run();
     }
+  }
 }
